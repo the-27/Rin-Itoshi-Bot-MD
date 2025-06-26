4a5b1c14a56477dea2e2cc65a7c35eed6635e8f1
the-27 <blackoficial2025@gmail.com>
2025-06-25 21:38:22 -0500

    Update descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -6,7 +6,7 @@ const youtubeRegexID = /(?:youtu\.be\/|youtube\.com\/(?:watch\?v=|embed\/))([a-z
 const handler = async (m, { conn, text, usedPrefix, command }) => {
   try {
     if (!text.trim()) {
-      return conn.reply(m.chat, `❀ Por favor, ingresa el nombre de la música a descargar.`, m)
+      return conn.reply(m.chat, `*🌴 Por favor, ingresa el nombre de la música a descargar.*`, m, fake)
     }
   
 let videoIdToFind = text.match(youtubeRegexID) || null
