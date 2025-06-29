196976367fc315762d271144ed5a136ce2988172
the-27 <blackoficial2025@gmail.com>
2025-06-29 15:11:46 -0500

    Update

--- plugins/owner-exec2.js
+++ plugins/owner-exec2.js
@@ -1,25 +1,28 @@
-import cp, {exec as _exec} from 'child_process';
-import {promisify} from 'util';
-const exec = promisify(_exec).bind(cp);
-const handler = async (m, {conn, isOwner, command, text, usedPrefix, args, isROwner}) => {
+import cp, { exec as _exec } from 'child_process';
+import { promisify } from 'util';
+const exec = promisify(_exec);
+
+const handler = async (m, { conn, isOwner, command, text, usedPrefix, args, isROwner }) => {
   if (!isROwner) return;
   if (global.conn.user.jid != conn.user.jid) return;
-  m.reply(`${emoji} *Ejecutando...*`);
+
+  m.reply('⚙️ *Ejecutando...*');
   let o;
   try {
-    o = await exec(command.trimStart() + ' ' + text.trimEnd());
+    o = await exec((command + ' ' + text).trim());
   } catch (e) {
     o = e;
   } finally {
-    const {stdout, stderr} = o;
-    if (stdout.trim()) m.reply(stdout);
-    if (stderr.trim()) m.reply(stderr);
+    const { stdout, stderr } = o;
+    if (stdout?.trim()) m.reply(stdout);
+    if (stderr?.trim()) m.reply(stderr);
   }
 };
-handler.help = ['$']
-handler.tags = ['owner']
-handler.customPrefix = ['$']
-handler.command = new RegExp
-handler.rowner = true
 
-export default handler
+handler.help = ['$'];
+handler.tags = ['owner'];
+handler.customPrefix = ['$'];
+handler.command = /^(.*)$/s;
+handler.rowner = true;
+
+export default handler;
