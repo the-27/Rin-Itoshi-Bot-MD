707cf63ce0e191a1e15028716d83bf63611276c1
the-27 <blackoficial2025@gmail.com>
2025-06-29 08:11:32 -0500

    Update 𝐑𝐈𝐍 𝐈𝐓𝐎𝐒𝐇𝐈

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,8 +1,10 @@
 let handler = async (m, { conn, usedPrefix }) => {
-  let creatorNumber = '51969214380'
-  let creatorName = '⚡ THE BLACK 🍁'
-  let github = 'https://github.com/the-27'
-  let imageUrl = 'https://files.catbox.moe/embijg.jpg'
+  let creatorNumber = '51969214380';
+  let creatorName = '⚡ THE BLACK 🍁';
+  let github = 'https://github.com/the-27';
+  let imageUrl = 'https://files.catbox.moe/embijg.jpg';
+
+  let userId = m.sender;
 
   let vcard = `
 BEGIN:VCARD
@@ -10,16 +12,17 @@ VERSION:3.0
 N:;${creatorName};;;
 FN:${creatorName}
 TEL;type=CELL;type=VOICE;waid=${creatorNumber}:${creatorNumber}
-END:VCARD`.trim()
+END:VCARD`.trim();
 
- 
+  
   await conn.sendMessage(m.chat, {
     contacts: {
       displayName: creatorName,
       contacts: [{ vcard }]
     }
-  }, { quoted: m })
-  
+  }, { quoted: m });
+
+ 
   let text = `┏━━━━━━━━━━━━━━━━━━━┓
 ┃ 🌹 *C R E A D O R - 💎 - B O T*
 ┣━━━━━━━━━━━━━━━━━━━┫
@@ -30,22 +33,21 @@ END:VCARD`.trim()
 ┗━━━━━━━━━━━━━━━━━━━┛
                     ᵉⁿˡᵃᶜᵉˢ ᵘᵗⁱˡᵉˢ`;
 
-  await conn.sendMessage(m.chat, {
-    image: { url: imageUrl },
-    caption: text,
+  await conn.sendMessage(m.chat, { 
+    text: text,
     contextInfo: {
+      mentionedJid: [userId],
       externalAdReply: {
-        showAdAttribution: true,
-        title: 'Información del creador',
+        title: '✧ Información del creador ✧',
         body: creatorName,
         thumbnailUrl: imageUrl,
-        sourceUrl: github,
         mediaType: 1,
+        showAdAttribution: true,
         renderLargerThumbnail: true
       }
     }
-  }, { quoted: m })
-}
+  }, { quoted: m });
+};
 
 handler.help = ["creador", "owner"];
 handler.tags = ["info"];
