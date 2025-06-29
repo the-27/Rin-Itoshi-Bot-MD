4c9e2a97296f4382b5b817d35033c3db59ee9602
the-27 <blackoficial2025@gmail.com>
2025-06-29 08:49:12 -0500

    Update 𝐑𝐈𝐍 𝐈𝐓𝐎𝐒𝐇𝐈

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -7,21 +7,6 @@ let handler = async (m, { conn, usedPrefix }) => {
     let taguser = '@' + userId.split("@s.whatsapp.net")[0];
     const name = conn.getName(m.sender);
 
-    const fkontak = {
-      key: {
-        participants: "0@s.whatsapp.net",
-        remoteJid: "status@broadcast",
-        fromMe: false,
-        id: "Halo"
-      },
-      message: {
-        contactMessage: {
-          displayName: name,
-          vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;${name};;;\nFN:${name}\nitem1.TEL;waid=${userId.split("@")[0]}:${userId.split("@")[0]}\nitem1.X-ABLabel:Mobile\nEND:VCARD`
-        }
-      },
-      participant: "0@s.whatsapp.net"
-    };
 
     let images = [
       'https://files.catbox.moe/pp7ncd.jpg',
@@ -40,35 +25,54 @@ let handler = async (m, { conn, usedPrefix }) => {
     const sections = [{
       title: "✦ MENÚS DISPONIBLES ✦",
       rows: [
-        { title: "📥 Menu Descargas", description: "Descargas desde redes sociales", id: `${usedPrefix}menudl` },
-        { title: "🎮 Menu RPG + Economía", description: "Sistema de aventuras y economía", id: `${usedPrefix}menurpg` },
-        { title: "🖼️ Menu busquedas", description: "MENU DE BUSQUEDAS, YOUTUBE, ECT", id: `${usedPrefix}menuse` },
-        { title: "🧰 Menu Herramientas", description: "Utilidades variadas", id: `${usedPrefix}menuherramientas` },
-        { title: "👤 Menu Perfil", description: "Opciones de perfil", id: `${usedPrefix}menuperfil` },
-        { title: "👥 Menu Grupos", description: "Administración de grupos", id: `${usedPrefix}menugrupo` },
-        { title: "🔍 Menu Búsquedas", description: "Buscar imágenes, videos y más", id: `${usedPrefix}menubusquedas` },
-        { title: "🔞 Menu +18", description: "Contenido NSFW", id: `${usedPrefix}menunsfw` },
+        { title: "📥 𝙈𝙚𝙣𝙪́ 𝙙𝙚 𝘿𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙨", description: "🎧 𝘿𝙚𝙨𝙘𝙖𝙧𝙜𝙖 𝙘𝙤𝙣𝙩𝙚𝙣𝙞𝙙𝙤 𝙙𝙚 𝙡𝙖𝙨 𝙥𝙧𝙞𝙣𝙘𝙞𝙥𝙖𝙡𝙚𝙨 𝙧𝙚𝙙𝙚𝙨: 𝙔𝙤𝙪𝙏𝙪𝙗𝙚, 𝙁𝙖𝙘𝙚𝙗𝙤𝙤𝙠, 𝙄𝙜, 𝙚𝙩𝙘.", id: `${usedPrefix}menudl` },
+        { title: "🧿 𝑴𝑬𝑵𝑼́ 𝑬𝑪𝑶𝑵𝑶𝑴𝑰́𝑨 + 𝑹𝑷𝑮 ⚔️", description: "🎮 𝘾𝙧𝙚𝙖 𝙩𝙪 𝙖𝙫𝙚𝙣𝙩𝙪𝙧𝙖, 𝙧𝙚𝙘𝙤𝙜𝙚 𝙧𝙚𝙘𝙪𝙧𝙨𝙤𝙨, 𝙜𝙖𝙣𝙖 𝙤𝙧𝙤 𝙮 𝙙𝙤𝙢𝙞𝙣𝙖 𝙚𝙡 𝙢𝙪𝙣𝙙𝙤 𝙍𝙋𝙂 ⚔️", id: `${usedPrefix}menurpg` },
+        { title: "🔍 𝐌𝐄𝐍𝐔́ 𝐃𝐄 𝐁𝐔́𝐒𝐐𝐔𝐄𝐃𝐀𝐒 🌐", description: "⟡ 𝐂𝐨𝐦𝐚𝐧𝐝𝐨𝐬 𝐞𝐬𝐩𝐞𝐜𝐢𝐚𝐥𝐞𝐬 𝐩𝐚𝐫𝐚 𝐛𝐮𝐬𝐜𝐚𝐫 𝐢𝐧𝐟𝐨𝐫𝐦𝐚𝐜𝐢𝐨́𝐧, 𝐚𝐮𝐝𝐢𝐨𝐬, 𝐯𝐢𝐝𝐞𝐨𝐬 𝐲 𝐦𝐮𝐜𝐡𝐨 𝐦𝐚́𝐬 𝐞𝐧 𝐥𝐢́𝐧𝐞𝐚 🌍", id: `${usedPrefix}menuse` },
+        { title: "👑 𝑴𝑬𝑵𝑼 𝑶𝑾𝑵𝑬𝑹 👑", description: "𝐂𝐨𝐧𝐭𝐫𝐨𝐥 𝐚𝐯𝐚𝐬𝐚𝐧𝐝𝐨 𝐩𝐚𝐫𝐚 𝐨𝐰𝐞𝐧𝐞𝐫𝐬", id: `${usedPrefix}dev` },
+        { title: "🎐 𝑴𝑬𝑵𝑼 𝑨𝑼𝑫𝑰𝑶𝑺🎚️", description: "𝐌𝐞𝐧𝐮 𝐚𝐮𝐝𝐢𝐨𝐬 𝐱𝐝", id: `${usedPrefix}menu2` },
+        { title: "👤 𝑴𝑬𝑵𝑼 / 𝑷𝑬𝑹𝑭𝑰𝑳", description: "🚀 𝐄𝐝𝐢𝐭𝐚 𝐭𝐮 𝐩𝐞𝐫𝐟𝐢𝐥 𝐚 𝐭𝐮 𝐠𝐮𝐬𝐭𝐨.", id: `${usedPrefix}perfildatesl` },
+        { title: "👥 𝑴𝑬𝑵𝑼 𝑮𝑹𝑼𝑷𝑶𝑺", description: "𝐀𝐝𝐦𝐢𝐧𝐢𝐬𝐭𝐫𝐚𝐜𝐢𝐨𝐧 𝐝𝐞 𝐠𝐫𝐮𝐩𝐨𝐬.", id: `${usedPrefix}menugp` },
+        { title: "🔞 𝑴𝑬𝑵𝑼 +18", description: "𝐜𝐨𝐧𝐭𝐞𝐧𝐢𝐝𝐨 𝐍𝐒𝐅𝐖", id: `${usedPrefix}menu18` },
+        { title: "❤️ 𝑴𝑬𝑵𝑼 𝑳𝑶𝑮𝑶𝑺", description: "👾 𝑴𝑬𝑵𝑼 𝑳𝑶𝑮𝑶𝑺𝑻𝑰𝑷𝑶𝑺 🎭.", id: `${usedPrefix}menulogos` },
       ]
     }];
 
     const interactiveMessage = {
       header: {
-        title: "HOLZ",
+        title: "🄷🄾🄻🄰",
         hasMediaAttachment: true,
         imageMessage: media.imageMessage
       },
       body: {
-        text: `✨ 𝐈𝐍𝐅𝐎 𝐔𝐒𝐔𝐀𝐑𝐈𝐎 ✨\n\n> 👤 Nombre: ${name}\n💠 Exp: ${exp}\n⭐ Nivel: ${level}\n🎖️ Rango: ${role}`
+        text: `｡ﾟ･ 𖥸──-ˋˏ❢ˎˊ-──𖥸 ｡ﾟ･
+> ✨ 𝐈𝐍𝐅𝐎 𝐔𝐒𝐔𝐀𝐑𝐈𝐎 ✨
+> ⚘۪۬👤 *USUARIO* : ${name}
+> ⚘۪۬💠 Exp: ${exp}
+> ⚘۪۬⭐ Nivel: ${level}
+> ⚘۪۬🎖️ Rango: ${role}
+> ┗━━━━━━━━━━━━━━━
+> ╭━═┅═━──────────◈
+> ┋➛  ⚙️ 𝐎𝐏𝐂𝐈𝐎𝐍𝐄𝐒: ⚙️
+> ┋
+> ┋⁖ฺ۟̇࣪·֗٬̤⃟⚙️ #menugp
+> ┋⁖ฺ۟̇࣪·֗٬̤⃟🎨 #menulogos
+> ┋⁖ฺ۟̇࣪·֗٬̤⃟💖 #dev
+> ┋⁖ฺ۟̇࣪·֗٬̤⃟☃️ #menu18
+> ┋⁖ฺ۟̇࣪·֗٬̤⃟💿 #menu2
+> ┋⁖ฺ۟̇࣪·֗٬̤⃟🔍 #menusearch
+> ┋⁖ฺ۟̇࣪·֗٬̤⃟📤 #menudl
+> ┋⁖ฺ۟̇࣪·֗٬̤⃟✨ #menurpg
+> ╰━═┅═━──────────◈`
       },
       footer: {
-        text: "⏤͟͞ू⃪ 𝑺𝑯𝑨𝑫𝑶𝑾 - 𝑩𝑶𝑻 • Powered by black"
+        text: "⏤͟͞ू⃪ 𝑹𝑰𝑵 𝑰𝑻𝑶𝑺𝑯𝑰 - 𝑩𝑶𝑻 • Powered by black"
       },
       nativeFlowMessage: {
         buttons: [
           {
             name: "single_select",
             buttonParamsJson: JSON.stringify({
-              title: "🔍 Selecciona un menú",
+              title: "ධ⃟🌹 𝐌𝐄𝐍𝐔 𝐋𝐈𝐒𝐓 🐉",
               sections
             })
           }
